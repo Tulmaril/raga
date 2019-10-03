60852b5f4dbd25b29492510594e4499d95cc97c6
Anton <asushakov@affmy.com>
2019-10-04 00:46:32 +0700

    +

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -11,33 +11,6 @@ class Blog extends Component {
         error: false
     }
 
-    componentDidMount () {
-
-
-
-        // console.log(localStorage.getItem('ragesAppRages'))
-
-        // if (localStorage.getItem('ragesAppRages') === null) {
-        //     localStorage.setItem('ragesAppRages')
-        // }
-        
-
-        // localStorage.setItem("storageFilm", JSON.stringify(favorites));
-    }
-
-
-    // function add(id, name, poster, overview) {
-    //     const favorites = JSON.parse(localStorage.getItem("storageFilm")) || {};
-    //     favorites[id] = [name, poster, overview,id];
-    //     localStorage.setItem("storageFilm", JSON.stringify(favorites));
-    //   }
-      
-    //   function remove(id) {
-    //     const favorites = JSON.parse(localStorage.getItem("storageFilm")) || {};
-    //     delete favorites[id];
-    //     localStorage.setItem("storageFilm", JSON.stringify(favorites));
-    //   }
-
     postSelectedHandler = (id) => {
         this.setState({selectedPostId: id});
     }
@@ -59,17 +32,6 @@ class Blog extends Component {
     }
 
     render () {
-
-        // let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        // if (!this.state.error) {
-        //     posts = this.state.posts.map(post => {
-        //         return <Post 
-        //             key={post.id} 
-        //             title={post.title} 
-        //             author={post.author}
-        //             clicked={() => this.postSelectedHandler(post.id)} />;
-        //     });
-        // }
 
         const rages = this.state.rages.map(rage => {
             return <Rage 
